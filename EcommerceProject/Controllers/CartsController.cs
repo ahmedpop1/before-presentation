f58925cb7e43abffc77093930c5d3ab7f9e20ba3
ahmedpop1 <48593343+ahmedpop1@users.noreply.github.com>
2022-08-21 05:59:13 +0200

    gfd

---
+++ EcommerceProject/Controllers/CartsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcommerceProject.models;
+using EcommerceProject.DTO;
+
+namespace EcommerceProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly EcommerceContext _context;
+
+        public CartsController(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Carts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cart>>> GetCarts()
+        {
+            return await _context.Carts.ToListAsync();
+        }
+
+        // GET: api/Carts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cart>> GetCart(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return cart;
+        }
+
+        // PUT: api/Carts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCart(int id, Cart cart)
+        {
+            if (id != cart.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cart).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Carts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cart>> PostCart(Cart cart)
+        {
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCart", new { id = cart.id }, cart);
+        }
+        [HttpPost("getitemstoken")]
+        public ActionResult<IEnumerable<Product>> getitemstoken([FromBody] CartDTO CartDTO)
+        {
+
+            var user = _context.Users.FirstOrDefault(d => d.token == CartDTO.token);
+            var cart = _context.Carts.FirstOrDefault(d => d.username == user.email);
+            if (user != null&&cart!=null)
+            {
+                var cartitems = _context.CartItems.Include(d => d.product).Where(d => d.CartId == cart.id).ToList();
+                List<Product> products = new List<Product>();
+                foreach (var item in cartitems)
+                {
+                    item.product.ImageSrc = "http://localhost:45421/Images/"+ item.product.ImageName;
+                    products.Add(item.product);
+                }
+                return products;
+            }
+            return BadRequest();
+        }
+
+        // DELETE: api/Carts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCart(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CartExists(int id)
+        {
+            return _context.Carts.Any(e => e.id == id);
+        }
+    }
+}
